eebafff7109eb27862a047de453c494e06ea9aa1
fitriandriy <202410102062@mail.unej.ac.id>
2024-05-06 21:28:52 +0700

    fix: routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const PeopleRouter = require('./peopleRouter')
+const views = require('./views')
 
 router.use('/people', PeopleRouter)
+router.use('/views', views)
 
 module.exports = router
